4bc353e333b24bdcbd4635fb822199a592457350
KristinaGovorova <k.govorova94@mail.ru>
2023-12-06 21:47:51 +0300

    p

--- src/main/java/ru/tele2/govorova/java/basic/homeworks/homework15/Employee.java
+++ src/main/java/ru/tele2/govorova/java/basic/homeworks/homework15/Employee.java
@@ -1,7 +1,6 @@
 package ru.tele2.govorova.java.basic.homeworks.homework15;
 
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.*;
 
 public class Employee {
 
@@ -30,16 +29,70 @@ public class Employee {
     public static void returnEmployeeOlderOrExactAge(ArrayList<Employee> list, int minimalAge) {
         ArrayList<Employee> orderedList = new ArrayList<Employee>();
         for (int i = 0; i < list.size(); i++) {
-            if (list.get(i).getAge() >= minimalAge){
+            if (list.get(i).getAge() >= minimalAge) {
                 orderedList.add(list.get(i));
             }
         }
         System.out.println(Arrays.asList(orderedList));
     }
 
+    public static void returnEmployeeOlderOrExactAge2(ArrayList<Employee> list, int minimalAge) {
+        Iterator<Employee> iter = list.iterator();
+        while (iter.hasNext()) {
+            Employee s = iter.next();
+            if (s.getAge() < minimalAge) {
+                iter.remove();
+            }
+        }
+        System.out.println(Arrays.asList(list));
+    }
+
+
+    public static boolean averageAge(ArrayList<Employee> list, int averageAge) {
+        int age = 0;
+        for (int i = 0; i < list.size(); i++) {
+            age += list.get(i).getAge();
+        }
+        age = age / list.size();
+        if (age > averageAge) {
+            System.out.println("Средний возраст " + age + ", т.е. больше минимального");
+            return true;
+        }
+        System.out.println("Средний возраст " + age + ", т.е. меньше минимального");
+        return false;
+    }
+
+    public static void younger(ArrayList<Employee> list) {
+        Iterator<Employee> iter = list.iterator();
+        int minAge = 200;
+        for (int i = 0; i < list.size(); i++)
+            if (list.get(i).getAge() < minAge) {
+                minAge = list.get(i).getAge();
+            }
+
+        while (iter.hasNext()) {
+            Employee s = iter.next();
+            if (s.getAge() != minAge) {
+                iter.remove();
+            }
+        }
+        System.out.println(Arrays.asList(list));
+    }
+
+    public static void getYoungerEmployee(ArrayList<Employee> list) {
+        Collections.sort(list, new Comparator<Employee>() {
+
+            @Override
+            public int compare(Employee t, Employee t1) {
+                return t.getAge() - t1.getAge();
+            }
+        });
+        System.out.println(list.get(0));
+    }
+
     @Override
     public String toString() {
-        return(name + " " + age + " ");
+        return (name + " " + age + " ");
     }
 
 }
