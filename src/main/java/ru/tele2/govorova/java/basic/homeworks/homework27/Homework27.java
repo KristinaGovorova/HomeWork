3623049e19a4724b415d9df6c53ef518c77936c4
KristinaGovorova <k.govorova94@mail.ru>
2024-07-22 12:19:15 +0300

    init

--- src/main/java/ru/tele2/govorova/java/basic/homeworks/homework27/Homework27.java
+++ src/main/java/ru/tele2/govorova/java/basic/homeworks/homework27/Homework27.java
@@ -6,37 +6,45 @@ import java.util.Scanner;
 
 public class Homework27 {
     public static void main(String[] args) {
-
+        String fileName;
+        String toSearch;
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите название файла, с которым хотите работать");
-        String fileName = scanner.next();
-        System.out.println("Введите искомую последовательность символов");
-        String toSearch = scanner.next();
-
-        File file = new File(fileName);
-        if (!(file.exists())) {
-            System.out.println("Данного файла не существует");
-        } else {
-            System.out.println("Последовательность " + toSearch + " повторяется " + countRepetitionsSequence(fileName, toSearch) + " раз(а)");
+        try {
+            System.out.println("Введите название файла, с которым хотите работать");
+            fileName = scanner.next();
+            System.out.println("Введите искомую последовательность символов");
+            toSearch = scanner.next();
+        } finally {
+            scanner.close();
         }
+        File file = new File(fileName);
+        if (!file.exists()) {
+            System.out.println("Данного файла не существует");
+            return;
+        }
+
+        System.out.println("Последовательность " + toSearch + " повторяется " + countRepetitionsSequence(fileName, toSearch) + " раз(а)");
+
     }
 
-    public static int countRepetitionsSequence(String fileName, String toSearch) {
+    public static int countRepetitionsSequence(File file, String toSearch) {
         int count = 0;
 
-        try (BufferedInputStream bufferedInputStream = new BufferedInputStream(new FileInputStream(fileName))) {
-            byte[] bytes = bufferedInputStream.readAllBytes();
-            String data = new String(bytes, StandardCharsets.UTF_8);
-            int indexOfSearch = data.indexOf(toSearch);
-            while (indexOfSearch != -1) {
-                count += 1;
-                indexOfSearch = data.indexOf(toSearch, indexOfSearch + toSearch.length());
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(file), StandardCharsets.UTF_8))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                int indexOfSearch = line.indexOf(toSearch);
+                while (indexOfSearch != -1) {
+                    count += 1;
+                    indexOfSearch = line.indexOf(toSearch, indexOfSearch + toSearch.length());
+                }
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
         return count;
     }
+}
 }
 
 
